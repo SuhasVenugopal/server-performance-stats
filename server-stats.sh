33d3ae21ce35fa1da0e698297bcd22c2267a6491
SuhasVenugopal <suhas@suhasiyengar85.>
2026-08-26 10:27:40 +0000

    Add CPU usage section

--- server-stats.sh
+++ server-stats.sh
@@ -2,4 +2,12 @@
 
 echo "============== Server Performance Stats =============="
 echo "Generated: $(date)"
-echo
+
+echo "============== CPU Usage ================"
+cpu_idle=$(top -bn1 | grep "Cpu(s)" | sed 's/.*, *\([0-9.]*\)%* id.*/\1/')
+echo "CPU Idle: ${cpu_idle}"
+cpu_usage=$(awk "BEGIN{printf \"%.1f\", 100 - $cpu_idle}")
+echo "CPU Usage: ${cpu_usage}"
+
+echo "============== Memory Usage ================"
+
