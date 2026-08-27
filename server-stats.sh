f22396a920090bccab409dff5eebb5b45addb0ff
SuhasVenugopal <suhas@suhasiyengar85.>
2026-08-27 13:37:15 +0000

    Add memory usage section

--- server-stats.sh
+++ server-stats.sh
@@ -7,7 +7,7 @@ echo "============== CPU Usage ================"
 cpu_idle=$(top -bn1 | grep "Cpu(s)" | sed 's/.*, *\([0-9.]*\)%* id.*/\1/')
 echo "CPU Idle: ${cpu_idle}"
 cpu_usage=$(awk "BEGIN{printf \"%.1f\", 100 - $cpu_idle}")
-echo "CPU Usage: ${cpu_usage}"
+echo "CPU Usage: ${cpu_usage}%"
 
 echo "============== Memory Usage ================"
-
+free -m | awk 'NR==2 {printf "Used: %s MB / Total: %s MB (%.1f%%)\nFree: %s MB\n", $3, $2, $3*100/$2, $4}'
